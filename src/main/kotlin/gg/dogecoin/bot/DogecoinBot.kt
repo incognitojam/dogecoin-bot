b2d0e8938f924b72976593d117e24cc0a1429e80
Cameron Clough <cameronjclough@gmail.com>
2021-03-04 12:05:58 +0000

    ✨ setup dotenv

--- src/main/kotlin/gg/dogecoin/bot/DogecoinBot.kt
+++ src/main/kotlin/gg/dogecoin/bot/DogecoinBot.kt
@@ -1,5 +1,10 @@
 package gg.dogecoin.bot
 
+import io.github.cdimascio.dotenv.dotenv
+
+val dotenv = dotenv()
+val CLIENT_ID: String = dotenv["CLIENT_ID"]
+
 fun main() {
-    println("Hello, world!")
+    println("Hello, world! Client ID: $CLIENT_ID")
 }
